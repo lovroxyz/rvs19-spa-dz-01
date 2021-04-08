f0cd0c4111ea289860e5f1923b773e338864b488
lovro krsul <81453938+lovroxyz@users.noreply.github.com>
2021-04-08 17:26:36 +0200

    Add files via upload

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,11 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include "cvjet.h"
+#include <vector>
+#include <iostream>
+
 
 int main()
 {
+
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
-
+	Cvjet cvijet(window);
+	cvijet.set_ran(false);
+	sf::Clock clock;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -14,9 +20,28 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
+		
+		
+		
+		cvijet.anim(clock);
+		std::cout << clock.getElapsedTime().asSeconds() <<"||"<<"\n";
+		
+	
 		window.clear();
-		//cvijet.draw();
+	
+		
+		
+		
+			
+		
+		window.draw(cvijet.get_stabljika());
+		for (int i = 0;i < 5;i++)
+		{
+			window.draw(cvijet.get_latice().at(i));
+		}
+		window.draw(cvijet.get_list());
+		window.draw(cvijet.get_tucak());
+	
 		window.display();
 	}
 
